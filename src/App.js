ebdacccbe5474ed8a6aeca18dd1b36045ec31a78
lorenzorinella <120283420+lorenzorinella@users.noreply.github.com>
2023-12-06 17:27:51 +0100

    added a working test of React Context

--- src/App.js
+++ src/App.js
@@ -2,31 +2,35 @@ import "./App.css";
 import { checlists } from "./components/data";
 import { useState } from "react";
 import ChecklistDisplay from "./components/ChecklistDisplay";
+import React from "react";
+import MyContext from "./components/Context";
 
 function App() {
   const [choice, setChoice] = useState(0);
   const checklistClick = (id) => {
     setChoice(id);
   };
-
+  const sharedData = "Ciccio";
   return (
-    <div className="App">
-      <h3>Hey we have some checklists: </h3>
-      <ul>
-        {checlists.map((list) => {
-          return (
-            <li>
-              <a key={list.id} onClick={() => checklistClick(list.id)}>
-                {list.title}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <div className="itemListContainer">
-        <ChecklistDisplay listnumber={choice} />
+    <MyContext.Provider value={sharedData}>
+      <div className="App">
+        <h3>Hey we have some checklists: </h3>
+        <ul>
+          {checlists.map((list) => {
+            return (
+              <li>
+                <a key={list.id} onClick={() => checklistClick(list.id)}>
+                  {list.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="itemListContainer">
+          <ChecklistDisplay listnumber={choice} />
+        </div>
       </div>
-    </div>
+    </MyContext.Provider>
   );
 }
 export default App;
--- src/components/ChecklistDisplay.js
+++ src/components/ChecklistDisplay.js
@@ -1,30 +1,37 @@
 import { checlists } from "./data";
+import React, { useContext } from "react";
+import MyContext from "./Context.js";
+
 export default function ChecklistDisplay(props) {
   const handleCheckboxChange = (event) => {
     const checkbox = event.target;
     const label = checkbox.nextSibling;
-
     if (checkbox.checked) {
       label.classList.add("barrato");
     } else {
       label.classList.remove("barrato");
     }
   };
+  const value = useContext(MyContext);
+  console.log(value);
   switch (props.listnumber) {
     case 0:
       return;
     case 1:
       return (
-        <ul>
-          {checlists[0].elements.map((element) => {
-            return (
-              <li>
-                <input type="checkbox" onChange={handleCheckboxChange} />
-                <label>{element}</label>
-              </li>
-            );
-          })}
-        </ul>
+        <>
+          <ul>
+            {checlists[0].elements.map((element) => {
+              return (
+                <li>
+                  <input type="checkbox" onChange={handleCheckboxChange} />
+                  <label>{element}</label>
+                </li>
+              );
+            })}
+          </ul>
+          <p>And this is my context value: {value}</p>
+        </>
       );
     case 2:
       return (
